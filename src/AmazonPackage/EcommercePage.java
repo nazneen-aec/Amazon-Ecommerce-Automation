7c996fa787f028884a79bff4d210070963f6a069
nazneen-wedoria <nazneen.fatma@wedoria.me>
2020-08-27 18:09:23 +0530

    added data files

--- src/AmazonPackage/EcommercePage.java
+++ src/AmazonPackage/EcommercePage.java
@@ -3,7 +3,12 @@ package AmazonPackage;
 import static io.appium.java_client.touch.TapOptions.tapOptions;
 import static io.appium.java_client.touch.offset.ElementOption.element;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -26,18 +31,43 @@ public class EcommercePage extends SettingsFile {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		Thread.sleep(5000);
 
+		File file = new File("src");
+		File newFile = new File(file, "dataFile.properties");
+
+		FileInputStream fileInput = null;
+
+		try {
+			fileInput = new FileInputStream(newFile);
+		}
+
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		Properties prop = new Properties();
+
+		// load properties file
+		try {
+			prop.load(fileInput);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		// To Login Amazon Site
+
 		driver.findElement(By.id("com.amazon.mShop.android.shopping:id/sign_in_button")).click();
 		Thread.sleep(5000);
 		driver.findElement(By.xpath(
 				"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.view.View[3]/android.widget.EditText"))
-				.sendKeys("9330024800");
-		;
+				.sendKeys(prop.getProperty("username"));
 		driver.findElementByXPath(
 				"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.view.View[4]/android.widget.Button")
 				.click();
-		driver.findElement(By.xpath("//android.widget.EditText[@text='Amazon password']")).sendKeys("admin#123");
+		driver.findElement(By.xpath("//android.widget.EditText[@text='Amazon password']"))
+				.sendKeys(prop.getProperty("password"));
 		driver.findElement(By.xpath("//android.widget.Button[@text='Login']")).click();
+
+		System.out.println("User name::" + prop.getProperty("username"));
+		System.out.println("Password::" + prop.getProperty("password"));
 
 		// ......Search Philips Tv from the list through scroll down.......
 
